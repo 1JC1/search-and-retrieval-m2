e8f0da3d711444153c24f60398f91e3b5fbce0b1
Enna Phan <ennap@uci.edu>
2023-02-22 10:34:34 -0800

    updated search

--- search.py
+++ search.py
@@ -1,8 +1,17 @@
+import json, os, re, sys, heapq
+from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import SnowballStemmer
+from Posting import Posting
+from indexer import indexer
+
 def process_query(query: str):
     # add stopword removal / recognition
     tokens_to_search = set()
     
-    for token in query.split():
+    stemmer = SnowballStemmer("english", ignore_stopwords=True)
+    
+    for token in word_tokenize(query):
         alphanum = re.sub(r'[^a-zA-Z0-9]', '', token)
         
         if len(alphanum) > 0:
@@ -11,5 +20,13 @@ def process_query(query: str):
     
     return tokens_to_search
 
-def search(tokens: lst[str]):
-    pass
+def search(tokens: set(str), index):
+    token_list = list(tokens).sort(key=lambda t: )
+    
+    for token in tokens:
+        index[token]
+        
+
+if __name__ == "__main__":
+    main_index = indexer()
+    
